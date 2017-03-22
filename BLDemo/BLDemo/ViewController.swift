7dc7e9552de995a33a60854193ecb291cd7eef74
Daniel.Tjuatja <daniel.tjuatja@gmail.com>
2017-03-22 09:22:52 +0800

    Initial project upload

---
+++ BLDemo/BLDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BLDemo
+//
+//  Created by Daniel on 22/3/17.
+//  Copyright © 2017 AppVania. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
