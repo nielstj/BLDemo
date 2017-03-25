d22d0540bf0de728439995ce02dd94e61860c14d
Daniel.Tjuatja <daniel.tjuatja@gmail.com>
2017-03-25 12:21:56 +0800

    Add outlet for player VC

--- BLDemo/BLDemo/Controllers/BLPlayerVC.swift
+++ BLDemo/BLDemo/Controllers/BLPlayerVC.swift
@@ -10,6 +10,17 @@ import Foundation
 import UIKit
 
 class BLPlayerVC: UIViewController {
+    
+    @IBOutlet weak var blurBG: UIImageView!
+    @IBOutlet weak var viewTitle: UILabel!
+    @IBOutlet weak var backBtn: UIButton!
+    @IBOutlet weak var entryImage: UIImageView!
+    @IBOutlet weak var titleLbl: UILabel!
+    @IBOutlet weak var sliderView: UIView!
+    @IBOutlet weak var playBtn: UIButton!
+    @IBOutlet weak var prevBtn: UIButton!
+    @IBOutlet weak var nextBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
